a28283e21f552e45e03109e2b840307d3b149121
jieyaozu <121235404@qq.com>
2015-09-13 16:15:09 +0800

    first commit

---
+++ foodienote/src/main/java/com/yaozu/foodienote/HomeMainActivity.java
@@ -0,0 +1,14 @@
+package com.yaozu.foodienote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class HomeMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_main);
+    }
+}
