01fdddb9b9564b1685ecf4b9308d9dcafd9e111c
jieyaozu <121235404@qq.com>
2015-09-13 16:31:00 +0800

    add imageView onclick event

--- foodienote/src/main/java/com/yaozu/foodienote/HomeMainActivity.java
+++ foodienote/src/main/java/com/yaozu/foodienote/HomeMainActivity.java
@@ -2,13 +2,24 @@ package com.yaozu.foodienote;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class HomeMainActivity extends Activity {
-
+    private ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_main);
+
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeMainActivity.this,"µã»÷ÁËÍ¼Æ¬",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
