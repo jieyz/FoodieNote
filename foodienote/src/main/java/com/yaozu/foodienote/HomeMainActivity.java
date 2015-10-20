356b165ee52bad57727a724b448d8574852a3b04
jieyz <jieyz@ifeng.com>
2015-10-20 09:15:08 +0800

    new build music bottom play bar

--- foodienote/src/main/java/com/yaozu/foodienote/HomeMainActivity.java
+++ foodienote/src/main/java/com/yaozu/foodienote/HomeMainActivity.java
@@ -4,13 +4,16 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 
+import com.yaozu.foodienote.activity.MusicHomeActivity;
 import com.yaozu.foodienote.fragment.DiscoverFragment;
 import com.yaozu.foodienote.fragment.HomeFragment;
 import com.yaozu.foodienote.fragment.MineFragment;
@@ -19,11 +22,10 @@ import com.yaozu.foodienote.fragment.RankFragment;
 
 
 public class HomeMainActivity extends Activity implements View.OnClickListener ,OnFragmentInteractionListener{
-    private RadioGroup mRadipGroup;
-    private RelativeLayout mFirst,mSecond,mThird,mFour;
     private RadioButton  mRadioFirst,mRadioSecond,mRadioThird,mRadioFour;
     private FragmentManager mFragmentManager;
     private String mCurrentFragmentTag;
+    private ImageButton music_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +37,12 @@ public class HomeMainActivity extends Activity implements View.OnClickListener ,
     }
 
     private void findViewByIds(){
-/*        mRadipGroup = (RadioGroup) findViewById(R.id.mb_tablelayout);
-        mFirst = (RelativeLayout) findViewById(R.id.rl_first);
-        mSecond = (RelativeLayout) findViewById(R.id.rl_second);
-        mThird = (RelativeLayout) findViewById(R.id.rl_third);
-        mFour = (RelativeLayout) findViewById(R.id.rl_four);*/
-
         mRadioFirst = (RadioButton) findViewById(R.id.radio_first);
         mRadioFirst.setChecked(true);
         mRadioSecond = (RadioButton) findViewById(R.id.radio_second);
         mRadioThird = (RadioButton) findViewById(R.id.radio_third);
         mRadioFour = (RadioButton) findViewById(R.id.radio_four);
+        music_home = (ImageButton) findViewById(R.id.music_home);
     }
 
     private void setOnclickLisener(){
@@ -53,6 +50,7 @@ public class HomeMainActivity extends Activity implements View.OnClickListener ,
         mRadioSecond.setOnClickListener(this);
         mRadioThird.setOnClickListener(this);
         mRadioFour.setOnClickListener(this);
+        music_home.setOnClickListener(this);
     }
 
     @Override
@@ -107,6 +105,10 @@ public class HomeMainActivity extends Activity implements View.OnClickListener ,
                     fragment = new MineFragment();
                 }
                 break;
+            case R.id.music_home:
+                Intent intent = new Intent(HomeMainActivity.this, MusicHomeActivity.class);
+                startActivity(intent);
+                return;
         }
         if(fragment.isAdded()){
            tr.show(fragment);
