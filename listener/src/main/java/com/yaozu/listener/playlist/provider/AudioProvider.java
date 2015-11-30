5d98ec0436a7df02a8dd491a806f922992e7a8a8
jieyaozu <121235404@qq.com>
2015-12-01 00:28:33 +0800

    modify bug

--- listener/src/main/java/com/yaozu/listener/playlist/provider/AudioProvider.java
+++ listener/src/main/java/com/yaozu/listener/playlist/provider/AudioProvider.java
@@ -18,6 +18,7 @@ import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
