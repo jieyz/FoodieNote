429a60a898a832b3bf752b047708abb60f0c6c2d
jieyaozu <121235404@qq.com>
2016-01-13 01:15:28 +0800

    adaptation difference android version

--- listener/src/main/jni/iconv/iconv.c
+++ listener/src/main/jni/iconv/iconv.c
@@ -151,6 +151,38 @@ static void put_32(unsigned char *s, unsigned c, int e)
 	s[e^3] = c;
 }
 
+size_t wcrtomb_s(char * s, wchar_t wc, mbstate_t * st)
+{
+	if (!s) return 1;
+	if ((unsigned)wc < 0x80) {
+		*s = wc;
+		return 1;
+	} else if ((unsigned)wc < 0x800) {
+		*s++ = 0xc0 | (wc>>6);
+		*s = 0x80 | (wc&0x3f);
+		return 2;
+	} else if ((unsigned)wc < 0xd800 || (unsigned)wc-0xe000 < 0x2000) {
+		*s++ = 0xe0 | (wc>>12);
+		*s++ = 0x80 | ((wc>>6)&0x3f);
+		*s = 0x80 | (wc&0x3f);
+		return 3;
+	} else if ((unsigned)wc-0x10000 < 0x100000) {
+		*s++ = 0xf0 | (wc>>18);
+		*s++ = 0x80 | ((wc>>12)&0x3f);
+		*s++ = 0x80 | ((wc>>6)&0x3f);
+		*s = 0x80 | (wc&0x3f);
+		return 4;
+	}
+	errno = EILSEQ;
+	return -1;
+}
+
+int wctomb(char *s, wchar_t wc)
+{
+	if (!s) return 0;
+	return wcrtomb_s(s, wc, 0);
+}
+
 /* Adapt as needed */
 #define mbrtowc_utf8 mbrtowc
 #define wctomb_utf8 wctomb
